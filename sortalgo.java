09cc7a4426c00b81a8b38d57a7e6a3984918ac69
meghrxj <hello.meghrajjj@gmail.com>
2021-10-24 18:21:38 +0530

    update

--- sortalgo.java
+++ sortalgo.java
@@ -1,9 +1,22 @@
-public class sortalgo{
+public class sortalgo {
 
     public static void main(String[] args){
 
+        int[] intArray = { };
 
-        
          
+    
     }
+    public static void swap(int[], int i, int j){
+
+        if(i == j){
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
 }
